6a966e6012ba781d03acc98da98f7eaf1883b2a2
user1896 <abde.user15@gmail.com>
2025-10-30 14:48:43 +0100

    adding a UserController to register users to the database

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,20 @@ class UserController extends Controller
 {
     public function register(Request $request)
     {
+        // Handle registration logic here
+        $incomingFields = $request->validate([
+            'name'=>'required',
+            'email'=>'required|email', // we can define multiple rules separated by |
+            'password'=>['required','min:3'], // we can also define rules as an array
+        ]);
+        //If the validation above fails, Laravel will automatically redirect back to the previous page.
+
+        $incomingFields['password'] = bcrypt($incomingFields['password']); // Hash the password before storing it
+        // We give the password field a new value, which is the hashed version of the password.
+        User::create($incomingFields); // Create a new user record in the users table of the database.
+        // User is an Eloquent model representing the "users" table that was created by the command "php artisan migrate".
+        
+        // For now, just return a simple message
         return "Hello from our controller";
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,5 +5,15 @@
 	<title>Document</title>
 </head>
 <body>
+	<div style="border: 3px solid black;">
+		<h2>Register</h2>
+		<form action="/register" method="POST">
+			@csrf
+			<input name="name" type="text" placeholder="name">
+			<input name="email" type="text" placeholder="email">
+			<input name="password" type="password" placeholder="password">
+			<button>Register</button>
+		</form>
+	</div>
 </body>
 </html>
